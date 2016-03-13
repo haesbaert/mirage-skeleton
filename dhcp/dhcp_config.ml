646a4c562690d9a365546abc091a43071504138c
Christiano Haesbaert <haesbaert@haesbaert.org>
2016-03-13 17:45:38 +0000

    Use charrua-dhcp-server as a default hostname.
    Stops ISC dhcpcd bitching about spaces on server name.

--- dhcp/dhcp_config.ml
+++ dhcp/dhcp_config.ml
@@ -4,7 +4,7 @@ let ip = Ipaddr.V4.of_string_exn
 let net = Ipaddr.V4.Prefix.of_string_exn
 let mac = Macaddr.of_string_exn
 
-let hostname =           "Charrua DHCP Server"
+let hostname =           "charrua-dhcp-server"
 let default_lease_time = 60 * 60 * 1 (* 1 hour *)
 let max_lease_time =     60 * 60 * 24 (* A day *)
 let ip_address =         ip "192.168.1.5"
